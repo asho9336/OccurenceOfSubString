91300b401e94d9317ba6f46a0f519fb901ed28bf
ahmadta <ahmadta@AHMADTA1.asiapacific.hpqcorp.net>
2016-04-29 20:12:13 +0530

    Initial Check in

---
+++ OccurenceOfSubString/src/com/ost/test/OOSSTest.java
@@ -0,0 +1,18 @@
+package com.ost.test;
+
+public class OOSSTest {
+	public static void main(String[] args) {
+		String str = "Hello Hello java Hello welcome me Hello welcome Hello";
+		int count=0;
+		String[] str2=str.split(" ");
+		
+		for(int i=0; i<str2.length; i++ ){
+			if(str2[i].equals("Hello")){
+				count++;
+			}
+		}
+			System.out.println("No of occurenceOf Hello : "+count);
+	}
+}
+
+
